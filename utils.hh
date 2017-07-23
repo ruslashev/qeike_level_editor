20d5915ba2ec9d632a430ec1529be1ea83b26f95
Ruslan Akbashev <ruslashev@gmail.com>
2017-07-23 19:16:13 +0300

    Get imgui to draw

---
+++ utils.hh
@@ -0,0 +1,42 @@
+#pragma once
+
+#include <fstream> // printf, puts, exit
+
+#ifdef __PRETTY_FUNCTION__
+#define info() \
+  printf("%s:%d in %s", __FILE__, __LINE__, __PRETTY_FUNCTION__);
+#else
+#define info() \
+  printf("%s:%d in %s", __FILE__, __LINE__, __func__);
+#endif
+
+#define assertf(X, ...) \
+  do { \
+    if (!(X)) { \
+      printf("assertion `%s' failed at ", #X); \
+      info() \
+      printf(": "); \
+      printf(__VA_ARGS__); \
+      puts(""); \
+      exit(1); \
+    } \
+  } while (0)
+
+#define die(...) do { printf(__VA_ARGS__); puts(""); exit(1); } while (0)
+
+#define warning(...) do { \
+  printf("warning: "); \
+  printf(__VA_ARGS__); \
+  puts(""); \
+} while (0)
+
+#define warning_ln(...) do { \
+  printf("warning at "); \
+  info() \
+  printf(": "); \
+  printf(__VA_ARGS__); \
+  puts(""); \
+} while (0)
+
+#define _glsl(X) "#version 120\n" #X
+
